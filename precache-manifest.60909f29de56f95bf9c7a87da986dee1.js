ad5f84530602258531c01c3df23ad412ed0c86f4
PSV37 <PSV37@users.noreply.github.com>
2025-05-15 08:18:14 +0000

    deploy: 4acded6f1e71371b8cdb04c6edb481fdbcb8ef30

---
+++ precache-manifest.60909f29de56f95bf9c7a87da986dee1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1c870e3d3ad1d89639a",
+    "url": "/me/static/css/main.129c9769.chunk.css"
+  },
+  {
+    "revision": "a1c870e3d3ad1d89639a",
+    "url": "/me/static/js/main.a1c870e3.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "fdf88007f19e8c8a5ae8c703a7e9ccdf",
+    "url": "/me/index.html"
+  }
+];
